f1d667fc4147d865f3ae9fa628bff8bb523129c4
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2026-01-02 15:02:57 +0000

    added wallet address labels for the dev fee and rplant

--- js/addressLabels.js
+++ js/addressLabels.js
@@ -1,6 +1,6 @@
 const addressLabels = {
     // MWC Wallets
-    "97eFcPMFYCyH2Y2qY4tsUS6CkYwTZdQ2am": "Dev Fee Wallet",
+    "98AUvgXPjAdKHj3FqwAPJNpd8XergXBL59": "Dev Fee Wallet",
     // "AKUg58E171GVJNw2RQzooQnuHs1zns2ecD": "Developer Wallet",
     // "AJzNjXgPYUGe9cmWz2ND7hrEYA1AiwpggX": "Community Wallet",
     // "Ac2DHE6freiBENuzZ3VTfY8zwwKc2oX2Fw": "Charity Wallet",
@@ -8,7 +8,7 @@ const addressLabels = {
     // // Pool addresses
     // "AREstbeSFzzbMGToUF6E2i3DbPa5nJB4Lz": "NovaGrid",
     // "Ae5vqtfRFKWYVfgCzyR4iat8FuKfLH4jve": "Coin-Miners",
-    // "AXRdunEc71n9oKLyLabAzV9eATRgkmGzMd": "RPlant",
+    "95iw269AkikMES6UcdC8xfHNqJXdXSxDzf": "RPlant",
     // "AKeNU8umLeCy4ZDJP5fkqM69VQEg4ydhka": "EvePool",
     // "AVayH8jK94vSHoden4UFjwnWqPGSEQvYpf": "ZergPool"
 };
